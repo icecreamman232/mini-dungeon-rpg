e6ba6855f475fce8d087ef8e5a2969484d821e1e
Stephen Ho <stephen.ho.232@gmail.com>
2023-12-22 23:18:13 +0700

    Add pick up vfx

--- Assets/_Scripts/LevelElements/Diamond.cs
+++ Assets/_Scripts/LevelElements/Diamond.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using JustGame.Scripts.Common;
 using JustGame.Scripts.Events;
 using JustGame.Scripts.Managers;
 using UnityEngine;
@@ -6,19 +8,37 @@ namespace JustGame.Scripts.LevelElements
 {
     public class Diamond : Item
     {
+        [SerializeField] private SpriteRenderer m_spriteRenderer;
+        [SerializeField] private AnimationParameter m_pickUpVFXAnim;
         [SerializeField] private IntEvent m_pickingDiamondEvent;
         [SerializeField] private int m_diamondValue;
+        
+        private bool m_isInProgress;
+        
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.gameObject.layer != LayerManager.PlayerLayer) return;
-
-            OnPicking();
+            if (other.gameObject.layer == LayerManager.PlayerLayer
+                && other.CompareTag("Player"))
+            {
+                OnPicking();
+            }
         }
 
         protected override void OnPicking()
         {
+            if (m_isInProgress) return;
+            StartCoroutine(PickingRoutine());
+        }
+        
+        private IEnumerator PickingRoutine()
+        {
+            m_isInProgress = true;
+            m_spriteRenderer.enabled = false;
+            m_pickUpVFXAnim.SetTrigger();
             m_pickingDiamondEvent.Raise(m_diamondValue);
+            yield return new WaitForSeconds(m_pickUpVFXAnim.Duration);
             base.OnPicking();
+            m_isInProgress = false;
         }
     }
 }
