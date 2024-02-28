70fe9503353e2e4d8d2be054e9668f7a11c3bccd
Stephen Ho <stephen.ho.232@gmail.com>
2024-02-29 00:37:17 +0700

    Rework door + add basic dungeon level layout

--- Assets/_Scripts/LevelLayout/Door.cs
+++ Assets/_Scripts/LevelLayout/Door.cs
@@ -3,7 +3,6 @@ using JustGame.Scripts.Common;
 using JustGame.Scripts.Events;
 using JustGame.Scripts.Managers;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace JustGame.Scripts.Levels
 {
@@ -12,16 +11,33 @@ namespace JustGame.Scripts.Levels
         [SerializeField] private AnimationParameter m_openAnim;
         [SerializeField] private BoolEvent m_finishRoomEvent;
         [SerializeField] private BoolEvent m_playerTravelToNewRoomEvent;
+        [SerializeField] private bool m_isLocked;
+        
+        [ContextMenu("Unlock")]
+        public void Unlock()
+        {
+            m_isLocked = false;
+            this.gameObject.layer = LayerManager.Interactible;
+        }
 
+        [ContextMenu("Lock")]
+        public void Lock()
+        {
+            m_isLocked = true;
+            this.gameObject.layer = LayerManager.Obstacle;
+        }
+        
         private void Start()
         {
+            Lock();
             m_finishRoomEvent.AddListener(OnFinishLevel);
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.gameObject.layer == LayerManager.PlayerLayer
-                && other.CompareTag("Player"))
+                && other.CompareTag("Player")
+                && !m_isLocked)
             {
                 StartCoroutine(PlayerTravelToNewRoom());
             }
@@ -30,6 +46,7 @@ namespace JustGame.Scripts.Levels
         private void OnFinishLevel(bool isFinished)
         {
             m_openAnim.SetBool(true);
+            Unlock();
         }
         
         private IEnumerator PlayerTravelToNewRoom()
